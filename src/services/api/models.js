3cf3166d30e354a460bb9140105bcd9a73fc387c
darthven <kostharl@gmail.com>
2018-07-13 18:00:45 +0300

    Useless shit was removed

--- src/services/api/models.js
+++ src/services/api/models.js
@@ -1,10 +1,5 @@
 import Joi from 'joi'
 
-export const fileUploadRequestSchema = Joi.object().keys({
-  base64: Joi.string().required().trim().min(4),
-  hashLength: Joi.number().required().min(6).max(32)
-})
-
 export const imageUploadRequestSchema = Joi.object().keys({
   base64: Joi.string().required().trim().min(4)
 })
